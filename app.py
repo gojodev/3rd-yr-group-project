fcb12acce1696bfc9904381b28e3c46cc1dd3a71
Morgan <48868958+MorganOnGitHub@users.noreply.github.com>
2024-09-18 11:52:06 +0100

    Refix flask app

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for the home page (index.html)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route for the about page (about.html)
 @app.route('/about')
 def about():
     return render_template('about.html')
