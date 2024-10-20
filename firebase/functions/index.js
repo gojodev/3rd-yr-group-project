e3c794e873e0118a25b75f8c7c8ab0f5aa3ab365
MorganOnGitHub <morgan.buckley.flynn@gmail.com>
2024-10-20 19:43:51 +0100

    Firebase initialisation

---
+++ firebase/functions/index.js
@@ -0,0 +1,45 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const { initializeApp } = require("firebase/app");
+const bcrypt = require('bcrypt');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFayRb90ywbg82EcLOnH5iBDm3qnZx9TU",
+    authDomain: "rd-year-project-1f41d.firebaseapp.com",
+    projectId: "rd-year-project-1f41d",
+    storageBucket: "rd-year-project-1f41d.appspot.com",
+    messagingSenderId: "823208675027",
+    appId: "1:823208675027:web:040ff96eac0fc89b0e3626",
+    measurementId: "G-86DQSH17PT"
+};
+
+initializeApp(firebaseConfig);
+
+const { getStorage, ref, getDownloadURL, uploadString } = require("firebase/storage");
+
+async function getRef_json(refItem) {
+    const url = await getDownloadURL(refItem);
+    const response = await fetch(url, { mode: 'cors' });
+    let data = await response.text();
+    data = JSON.parse(data);
+    return data;
+}
+
+// ! global
+const storage = getStorage();
+const userCreds = ref(storage, 'A_userCreds.json');
+
+async function loadInfo() {
+    return await Promise.resolve(getRef_json(userCreds));
+}
+
+exports.showDB = onRequest({ 'region': 'europe-west2' }, async (req, res) => {
+    try {
+        let db = await loadInfo();
+        res.json(db);
+
+    }
+    catch (error) {
+        console.log('Couldnt access the database: ', error)
+        res.status(500).json({ error: "Interal server error" })
+    }
+});
